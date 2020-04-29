0ba1de242563fcfe13182cc60adf61eb5a46d897
rohinisharma <rasberry314@gmail.com>
2020-04-29 04:59:04 -0400

    Created KNN constructors

--- src/KNN_Model.cpp
+++ src/KNN_Model.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "KNN_Model.h"
+#include<opencv2/ml/ml.hpp>
+
+using cv::ml::KNearest;
+using cv::Ptr;
+
+namespace ocr {
+
+KNN_Model::KNN_Model() {
+  Ptr<KNearest> kNearest_model_(KNearest::create());
+}
+
+KNN_Model::KNN_Model(const string& saved_model) {
+  Ptr<KNearest> kNearest_model_(KNearest::load(saved_model));
+}
+
+
+}
--- src/KNN_Model.h
+++ src/KNN_Model.h
@@ -4,6 +4,13 @@
 
 #ifndef FINALPROJECT_KNN_MODEL_H
 #define FINALPROJECT_KNN_MODEL_H
+#include <opencv2/ml.hpp>
+#include <string>
+#include <ocr/Character.h>
+
+using cv::ml::KNearest;
+using std::string;
+using ocr::Character;
 
 namespace ocr {
 
@@ -11,11 +18,12 @@ class KNN_Model {
  public:
   KNN_Model();
   KNN_Model(const string& saved_model_path);
-  Train(const string& training_img_dir, const string& label_path);
+  void Train(const string& training_img_dir, const string& label_path);
 
   string ClassifyImage(const string& image_path);
 
  private:
+  cv::Ptr<KNearest> kNearest_model_;
   string ClassifySingleCharacter(const Character& character_to_classify);
 
 
