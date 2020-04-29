84d931a8aa5689594ad3bdba9c7154633d872f10
rohinisharma <rasberry314@gmail.com>
2020-04-28 23:01:33 -0400

    Added test cases for GetCharacters

--- tests/training_utils_test.cpp
+++ tests/training_utils_test.cpp
@@ -1,3 +1,23 @@
 //
 // Created by Rohini Sharma on 4/28/20.
 //
+
+#include <catch2/catch.hpp>
+#include <ocr/training_utils.h>
+
+using ocr::GetCharacters;
+
+TEST_CASE("Invalid file paths") {
+  SECTION("Invalid image directory") {
+    string training_imgs = "bad/path";
+    string training_label = "../../../../../../tests/assets/test_labels.txt";
+    REQUIRE_THROWS(GetCharacters(training_imgs, training_label));
+  }
+
+  SECTION("Invalid label file path") {
+    string training_imgs = "../../../../../../tests/assets/test_images/";
+    string training_label = "bad/path";
+    REQUIRE_THROWS(GetCharacters(training_imgs, training_label));
+  }
+
+}
