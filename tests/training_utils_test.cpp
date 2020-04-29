6f066a8e78966f35bde445b128c2a7df6b25bdab
rohinisharma <rasberry314@gmail.com>
2020-04-29 03:45:00 -0400

    Added flattened mat test cases

--- tests/training_utils_test.cpp
+++ tests/training_utils_test.cpp
@@ -5,10 +5,12 @@
 #include <catch2/catch.hpp>
 #include <ocr/training_utils.h>
 #include <ocr/Character.h>
+#include <iostream>
 
 using ocr::GetCharacters;
 using ocr::LabeledCharacter;
 using ocr::GetNumericalLabelsMat;
+using ocr::GetFlattenedImagesMat;
 
 TEST_CASE("Invalid file paths") {
   SECTION("Invalid image directory") {
@@ -45,4 +47,18 @@ TEST_CASE("Numerical Labels Mat Test") {
   Mat expected = Mat(1, 5, CV_32FC1, data);
   Mat differences = GetNumericalLabelsMat(train_char) != expected;
   REQUIRE(cv::countNonZero(differences) == 0);
+}
+
+TEST_CASE("Flattened images mat test") {
+  string training_imgs = "../../../../../../tests/assets/test_images/";
+  string training_label = "../../../../../../tests/assets/test_labels.txt";
+  vector<LabeledCharacter> train_char = GetCharacters(training_imgs, training_label);
+  Mat flattened_imgs = GetFlattenedImagesMat(train_char);
+  SECTION("Flattened image matrix has correct number of rows") {
+    REQUIRE(flattened_imgs.rows == 5);
+  }
+
+  SECTION("Flattened image matrix has correct number of columns") {
+    REQUIRE(flattened_imgs.cols == 900);
+  }
 }
